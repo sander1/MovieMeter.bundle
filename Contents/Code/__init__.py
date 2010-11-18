fd00eaa1460790bf781df6f3342c80efb6f86e82
sander1 <sanderspies+plexapp@gmail.com>
2010-11-19 03:38:45 +0800

    Grab title and summary from the website instead of the API output

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,6 +3,7 @@ from time import time
 
 MM_ENDPOINT_URI = 'http://www.moviemeter.nl/ws'
 MM_API_KEY = 'fnsxd7shhmc8gefjhz2zv0nrjwjbezhj'
+MM_MOVIE_PAGE = 'http://www.moviemeter.nl/film/%d'
 
 def Start():
   HTTP.CacheTime = CACHE_1DAY
@@ -26,15 +27,16 @@ class MovieMeterAgent(Agent.Movies):
   def update(self, metadata, media, lang):
     response = self.proxy.film.retrieveDetails(self.get_session_key(), int(metadata.id))
     if response != None:
-      metadata.title = unicode(response['title']).replace(u'\u0092', u'\u2019')
       metadata.year = int(response['year'])
-      metadata.summary = unicode(response['plot']).replace(u'\u0092', u'\u2019')
       metadata.rating = float(response['average'])*2 # Max 5 for MovieMeter, needs max 10 for Plex
+
+      movie_page = HTML.ElementFromURL(MM_MOVIE_PAGE % int(metadata.id))
+      metadata.title = movie_page.xpath('//div[@id="centrecontent"]/h1')[0].text.rsplit('(',1)[0].strip()
+      metadata.summary = movie_page.xpath('//div[@id="film_info"]/text()[last()]')[0].strip()
 
   def get_session_key(self):
     if self.valid_till < int(time()):
       response = self.proxy.api.startSession(MM_API_KEY)
       self.session_key = response['session_key']
       self.valid_till = int(response['valid_till'])
-
     return self.session_key
